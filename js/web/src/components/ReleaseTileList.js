cb2a59c329ef6beb4f464fcf97e1ae9c256b5db1
Eric Farber <eric.j.farber@gmail.com>
2021-11-23 17:38:39 -0500

    format

--- js/web/src/components/ReleaseTileList.js
+++ js/web/src/components/ReleaseTileList.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { styled } from '@mui/material/styles'
 import ninaCommon from 'nina-common'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import { Typography, Box } from '@mui/material'
 import SmoothImage from 'react-smooth-image'
 import PlayCircleOutlineOutlinedIcon from '@mui/icons-material/PlayCircleOutlineOutlined'
@@ -20,7 +20,6 @@ const ReleaseTileList = (props) => {
     history.push(`/${releasePubkey}`)
   }
 
-
   return (
     <Box>
       <TileGrid>
@@ -28,18 +27,20 @@ const ReleaseTileList = (props) => {
           return (
             <Tile key={i}>
               <HoverCard>
-                <CardCta onClick={() => {
-                  handleClick(release.releasePubkey)
-                }}>
+                <CardCta
+                  onClick={() => {
+                    handleClick(release.releasePubkey)
+                  }}
+                >
                   <Button
-                    onClick={(e) =>{
+                    onClick={(e) => {
                       e.stopPropagation()
                       updateTxid(
                         release.metadata.properties.files[0].uri,
                         release.releasePubkey,
                         true
-                      )}
-                    }
+                      )
+                    }}
                   >
                     <PlayCircleOutlineOutlinedIcon sx={{ color: 'white' }} />
                   </Button>
@@ -111,7 +112,7 @@ const CardCta = styled(Box)(({ theme }) => ({
   alignItems: 'center',
   '&:hover': {
     opacity: '1',
-    cursor: 'pointer'
+    cursor: 'pointer',
   },
 }))
 
