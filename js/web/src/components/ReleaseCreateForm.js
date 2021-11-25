bf1f073d15b2701551d636ee27298ce8bf1c6e69
Eric Farber <eric.j.farber@gmail.com>
2021-11-25 14:41:51 -0500

    reset pressingState on cancelled TX

--- js/web/src/components/ReleaseCreateForm.js
+++ js/web/src/components/ReleaseCreateForm.js
@@ -170,7 +170,7 @@ const ReleaseCreateForm = ({
           </Typography>
           <Box>
             <Slider
-              defaultValue={20}
+              defaultValue={10}
               getAriaValueText={valuetext}
               aria-labelledby="percent"
               className={classes.formField}
@@ -231,7 +231,7 @@ export default withFormik({
       catalogNumber: '',
       amount: undefined,
       retailPrice: undefined,
-      resalePercentage: 20,
+      resalePercentage: 10,
     }
   },
 })(ReleaseCreateForm)
