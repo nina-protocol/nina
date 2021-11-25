ee77497f42f0d4e573e3ea96f9e65243a6df3091
Eric Farber <eric.j.farber@gmail.com>
2021-11-25 14:51:12 -0500

    lint+format

--- js/web/src/components/ReleaseCreateForm.js
+++ js/web/src/components/ReleaseCreateForm.js
@@ -170,7 +170,7 @@ const ReleaseCreateForm = ({
           </Typography>
           <Box>
             <Slider
-              defaultValue={10}
+              defaultValue={0}
               getAriaValueText={valuetext}
               aria-labelledby="percent"
               className={classes.formField}
@@ -231,7 +231,7 @@ export default withFormik({
       catalogNumber: '',
       amount: undefined,
       retailPrice: undefined,
-      resalePercentage: 10,
+      resalePercentage: 0,
     }
   },
 })(ReleaseCreateForm)
