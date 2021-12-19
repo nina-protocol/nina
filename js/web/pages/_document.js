5980583ead38fa662f7fec460e49c3cc79b8d21e
facing-n <mike@nina.market>
2021-12-19 09:59:54 -0500

    add Head to Layout

--- js/web/pages/_document.js
+++ js/web/pages/_document.js
@@ -41,67 +41,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" dir="ltr">
-        <Head>
-          <meta content="text/html; charset=UTF-8" name="Content-Type" />
-          <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-          <meta name="theme-color" content="#000000" />
-          <meta
-            name="description"
-            content="Nina: A new way to publish, stream, and purchase music."
-          />
-          <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-          <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-          <script src="https://cdn.dashjs.org/v3.2.1/dash.all.min.js"></script>
-
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          />
-
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="shortcut icon"
-            type="image/png"
-            sizes="32x32"
-            href="/public/favicon-32x32.png"
-          />
-          <link
-            rel="shortcut icon"
-            type="image/png"
-            sizes="16x16"
-            href="/public/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-VDD58V1D22"
-          ></script>
-          {() => {
-            window.dataLayer = window.dataLayer || [];
-            const gtag = () => window.dataLayer.push(arguments);
-            gtag("js", new Date());
-            gtag("config", "G-VDD58V1D22");
-          }}
-        </Head>
         <body style={{ margin: "0px", position: "relative" }}>
           <Main />
           <NextScript />
