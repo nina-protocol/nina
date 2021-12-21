34fe813f7db3d7daa533be04230cf1bd7b0992c1
Eric Farber <eric.j.farber@gmail.com>
2021-12-21 16:35:35 -0500

    fix player image

--- js/web/components/AudioPlayer.js
+++ js/web/components/AudioPlayer.js
@@ -191,10 +191,12 @@ const AudioPlayer = () => {
       </audio>
 
       {info && (
-        <AlbumArt href={`/${info.releasePubkey}`}>
-          <Image src={info.cover} height="60px" width="60px" />
-        </AlbumArt>
-      )}
+        <Link href={`/${info.releasePubkey}`}>
+          <AlbumArt >
+            <Image src={info.cover} height="60px" width="60px" layout='responsive'/>
+          </AlbumArt>
+        </Link>
+        )}
 
       <Controls>
         <IconButton
@@ -311,7 +313,7 @@ const StyledAudioPlayer = styled(Box)(({ theme }) => ({
   zIndex: "100",
 }));
 
-const AlbumArt = styled(Link)(() => ({
+const AlbumArt = styled(Box)(() => ({
   width: "60px",
   height: "60px",
 }));
