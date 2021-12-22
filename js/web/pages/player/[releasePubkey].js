bb14cfb1704a85dc8060d072c0f485462b43bde8
facing-n <mike@nina.market>
2021-12-21 20:33:43 -0500

    Twitter card (#131)
    * pushing for help
    
    * add seek + title + overlay to twitter card

--- js/web/pages/player/[releasePubkey].js
+++ js/web/pages/player/[releasePubkey].js
@@ -7,31 +7,81 @@ const ReleaseEmbedPage = ({ host, metadata }) => {
       <head>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <style>
-        body {
-          margin: 0px;
-        }
-          .container {
+          body {
+            margin: 0px;
+          }
+
+          #container {
             height: 400px;
             width: 100%;
             position: relative;
+            flex-direction: column;
           }
           
-          .vertical-center {
+          #vertical-center {
             margin: 0;
             position: absolute;
             top: 50%;
             left: 50%;
             -ms-transform: translate(-50%, -50%);
             transform: translate(-50%, -50%);
+            display: none;
+          }
+
+          #seekbar-container {
+            width: 100%;
+            height: 8px;
+            z-index: 10;
+            background-color: black;
+          }
+
+          #seekbar {
+            width: 0%;
+            height: 100%;
+            background-color: #2D81FF;
+            z-index: 100;
+            position: relative;
+          }
+
+          #image {
+            z-index: -1;
+          }
+
+          #overlay {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-color: black;
+            display: none;
+            opacity: 0.5;
+          }
+
+          #title {
+            display: block;
+            position: absolute;
+            z-index: 1000;
+            color: white;
+            font-family: helvetica;
+            margin: 0 auto;
+            width: 100%;
+            text-align: center;
+            display: none;
           }
         </style>
       </head>
       <body>
-        <div class="container">
-          <div class="vertical-center">
-            <input id="player-button" type="image" src="https://${host}/play.svg" width="80px height="80px/>
+        <div id="container">
+          <div id="overlay"></div>
+          <div id="title">
+            <h4>NINA</h4>
           </div>
-          <img id="image" src=${metadata.image} height="100%" width="100%"/>
+          <div id="vertical-center">
+            <input id="player-button" type="image" src="https://${host}/play.svg" width="40px height="40px />
+          </div>
+          <img id="image" src=${metadata.image} height="100%" width="100%" />
+          <div id="seekbar-container">
+            <div id="seekbar"></div>
+          </div>
           <audio id="nina-player" style={{ width: "100%" }} autoplay>
             <source src=${metadata.animation_url} type="audio/mp3" />
           </audio>
@@ -40,10 +90,24 @@ const ReleaseEmbedPage = ({ host, metadata }) => {
       <script>
         $(document).ready(function () {
             // Bind the DIV element to the .click() method.
+            $("#overlay").click(function () {
+              let player = $('#nina-player')[0];
+              player[player.paused ? 'play' : 'pause']();
+            });
             $("#player-button").click(function () {
               let player = $('#nina-player')[0];
               player[player.paused ? 'play' : 'pause']();
             });
+            $('#container').mouseenter(function() {
+              $('#vertical-center').eq(0).show();
+              $('#overlay').eq(0).show();
+              $('#title').eq(0).show();
+            })
+            $('#container').mouseleave(function() {
+              $('#vertical-center').eq(0).hide();
+              $('#overlay').eq(0).hide();
+              $('#title').eq(0).hide();
+            })
             $('#nina-player').on("play", function() {
               let playerButton = $('#player-button')[0];
               playerButton.src = "https://${host}/pause.svg"
@@ -52,6 +116,16 @@ const ReleaseEmbedPage = ({ host, metadata }) => {
               let playerButton = $('#player-button')[0];
               playerButton.src = "https://${host}/play.svg"
             })
+            $('#nina-player').on('timeupdate', function() {
+              $('#seekbar')[0].style.width = ((this.currentTime / this.duration) * 100) + "%";
+            });
+            $("#seekbar-container").click(function (e) {
+              let player = $('#nina-player')[0];
+              let width = $('#seekbar-container')[0].offsetWidth
+              let position = e.originalEvent.clientX
+              let newTime = (position / width) * player.duration
+              player.currentTime = newTime
+            });
         });
       </script>
     </html>
@@ -61,7 +135,7 @@ const ReleaseEmbedPage = ({ host, metadata }) => {
     <iframe
       id="nina-player"
       width="100%"
-      height="400px"
+      height="410px"
       style={{ border: "none" }}
       src={dataURI}
     />
