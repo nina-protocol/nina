c5e28c7633a6e384d555dd67364e597f0b63a3e7
Eric Farber <eric.j.farber@gmail.com>
2021-12-23 10:52:53 -0500

    link

--- js/web/pages/_document.js
+++ js/web/pages/_document.js
@@ -59,19 +59,19 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/images/apple-touch-icon.png"
           />
           <link
             rel="shortcut icon"
             type="image/png"
             sizes="32x32"
-            href="/public/favicon-32x32.png"
+            href="/images/favicon-32x32.png"
           />
           <link
             rel="shortcut icon"
             type="image/png"
             sizes="16x16"
-            href="/public/favicon-16x16.png"
+            href="/images/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
 
