4f8d891180a28868cba0faca5e754aa746f71a09
facing-n <mike@nina.market>
2021-12-28 11:09:45 -0500

    initial radio commit

---
+++ js/nina-radio/components/Layout.js
@@ -0,0 +1,52 @@
+import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Root>
+        <CssBaseline />
+        <Container
+          maxWidth={true}
+          disableGutters
+          className={classes.mainContainer}
+        >
+          <main className={classes.bodyContainer}>{children}</main>
+        </Container>
+      </Root>
+    </>
+  );
+};
+
+const PREFIX = "Layout";
+
+const classes = {
+  mainContainer: `${PREFIX}-mainContainer`,
+  bodyContainer: `${PREFIX}-bodyContainer`,
+};
+
+const Root = styled("div")(({theme}) => ({
+  [`& .${classes.mainContainer}`]: {
+    minHeight: "100vh",
+    height: "100vh",
+    width: "100vw",
+    overflowX: "hidden",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+
+  [`& .${classes.bodyContainer}`]: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    // textAlign: "center",
+    height: "100%",
+    paddingLeft: theme.spacing(1)
+  },
+}));
+
+export default Layout;
