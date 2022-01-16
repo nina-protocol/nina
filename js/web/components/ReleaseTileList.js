981e91700ff050d948634f14341edbe7fdba7bc3
facing-n <mike@nina.market>
2022-01-16 10:54:10 -0500

    limit length of artist/title on all releases / release

--- js/web/components/ReleaseTileList.js
+++ js/web/components/ReleaseTileList.js
@@ -84,7 +84,7 @@ const ReleaseTileList = (props) => {
                     ? `${release.tokenData.remainingSupply.toNumber()} / ${release.tokenData.totalSupply.toNumber()} remaining`
                     : "Sold Out"}
                 </Typography>
-                <ReleaseName>{release.metadata.name}</ReleaseName>
+                <ReleaseName>{release.metadata.name.substring(0, 100)}</ReleaseName>
               </Box>
             </Tile>
           );
