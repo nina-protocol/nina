eb235456b322cf83caab51397e5281013f89fe66
Eric Farber <eric.j.farber@gmail.com>
2022-04-22 15:06:03 -0400

    remove publishedby lookup type

--- js/nina-common/src/contexts/release.js
+++ js/nina-common/src/contexts/release.js
@@ -902,8 +902,7 @@ const releaseContextHelper = ({
   }
 
   const getReleasesPublishedByUser = async (publicKey) => {
-    await getReleasesHandler(publicKey, lookupTypes.REVENUE_SHARE)
-    await getReleasesHandler(publicKey, lookupTypes.PUBLISHED_BY)
+    await getReleasesHandler(publicKey, lookupTypes.REVENUE_SHARE) 
   }
 
   const getRelatedForRelease = async (releasePubkey) => {
