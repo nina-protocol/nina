ae102c301acf0df005faea3b558de287fe7b5925
Eric Farber <eric.j.farber@gmail.com>
2022-05-18 12:28:02 -0400

    remove-resolve

--- js/hubs/next.config.js
+++ js/hubs/next.config.js
@@ -21,7 +21,6 @@ module.exports = withBundleAnalyzer({
     }
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@nina-protocol/nina-sdk': path.resolve('../sdk'),
       path: require.resolve('path-browserify'),
       react: path.resolve('../node_modules/react'),
       crypto: require.resolve('crypto-browserify'),
