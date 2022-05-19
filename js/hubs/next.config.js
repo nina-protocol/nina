ed3dc99715576235823a06860ed9fa718d6675a4
Eric Farber <eric.j.farber@gmail.com>
2022-05-19 17:36:20 -0400

    sync

--- js/hubs/next.config.js
+++ js/hubs/next.config.js
@@ -48,6 +48,7 @@ module.exports = withBundleAnalyzer({
       cluster === 'devnet'
         ? 'https://nina.devnet.rpcpool.com'
         : 'https://nina.rpcpool.com',
+    REACT_HUB_PUBLIC_KEY: 'HsWutvySaqWXT67qfoJ8xZFtPFa1SorDPTJSyndMf7Ld',
     REACT_PROGRAM_ID:
       cluster === 'devnet'
         ? '77BKtqWTbTRxj5eZPuFbeXjx3qz4TTHoXRnpCejYWiQH'
