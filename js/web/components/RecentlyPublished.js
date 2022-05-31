25950e807367dd24c3b7a040b4001cbec552853d
facing-n <mike@nina.market>
2022-05-31 16:27:21 -0400

    Update RecentlyPublished.js (#209)
    RecentlyPublished Image.priority should be true so that

--- js/web/components/RecentlyPublished.js
+++ js/web/components/RecentlyPublished.js
@@ -94,7 +94,7 @@ const RecentlyPublished = (props) => {
                         height={100}
                         width={100}
                         layout="responsive"
-                        priority={false}
+                        priority={true}
                         release={release}
                       />
                     </a>
