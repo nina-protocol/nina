74db4fb158de84d2cbd2149ac06775dd1a34b52f
Eric Farber <eric.j.farber@gmail.com>
2022-06-09 17:57:56 -0400

    test

--- js/web/pages/_document.js
+++ js/web/pages/_document.js
@@ -1,113 +1,94 @@
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable @next/next/no-sync-scripts */
-import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
-import ServerStyleSheets from '@mui/styles/ServerStyleSheets'
-// import createEmotionServer from '@emotion/server/create-instance';
-// import createEmotionCache from '../src/createEmotionCache';
-// import {styled} from '@mui/material/styles'
+import * as React from 'react';
+import Document, {Html, Head, Main, NextScript} from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
 
-// const sheets = new ServerStyleSheets();
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const styledComponentsSheet = new ServerStyleSheet()
-    const materialSheets = new ServerStyleSheets()
-    const originalRenderPage = ctx.renderPage
+import createCache from '@emotion/cache';
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            styledComponentsSheet.collectStyles(
-              materialSheets.collect(<App {...props} />)
-            ),
-        })
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <React.Fragment>
-            {initialProps.styles}
-            {materialSheets.getStyleElement()}
-            {styledComponentsSheet.getStyleElement()}
-          </React.Fragment>
-        ),
-      }
-    } finally {
-      styledComponentsSheet.seal()
-    }
-  }
+// prepend: true moves MUI styles to the top of the <head> so they're loaded first.
+// It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
+function createEmotionCache() {
+  return createCache({key: 'css', prepend: true});
+}
 
+export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <meta name="Content-Type" content="text/html; charset=UTF-8" />
-          <meta name="theme-color" content="#000000" />
-          <link rel="icon" href="/images/favicon.ico" />
-          <link rel="apple-touch-icon" href="/images/logo192.png" />
-          <link rel="manifest" href="/manifest.json" />
+          {/* PWA primary color */}
+          <meta name="theme-color" content={'#ffffff'} />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          />
-
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/images/apple-touch-icon.png"
-          />
-          <link
-            rel="shortcut icon"
-            type="image/png"
-            sizes="32x32"
-            href="/images/favicon-32x32.png"
-          />
-          <link
-            rel="shortcut icon"
-            type="image/png"
-            sizes="16x16"
-            href="/images/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
-          <script src="https://cdn.dashjs.org/v3.2.1/dash.all.min.js" />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-VDD58V1D22"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-VDD58V1D22', { page_path: window.location.pathname });
-              `,
-            }}
-          />
+          {/* Inject MUI styles first to match with the prepend: true configuration. */}
+          {this.props.emotionStyleTags}
         </Head>
-        <body style={{ margin: '0px', position: 'relative' }}>
+        <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
-export default MyDocument
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  const originalRenderPage = ctx.renderPage;
+
+  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createEmotionCache();
+  const {extractCriticalToChunks} = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  // This is important. It prevents emotion to render invalid HTML.
+  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{__html: style.css}}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  };
+};
