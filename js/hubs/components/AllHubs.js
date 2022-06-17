04ed5efd732bbf78f07620427368ce8cc230ff2b
Eric Farber <eric.j.farber@gmail.com>
2022-06-17 18:42:21 -0400

    pause

--- js/hubs/components/AllHubs.js
+++ js/hubs/components/AllHubs.js
@@ -3,12 +3,14 @@ import nina from '@nina-protocol/nina-sdk'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
 import Link from '@mui/material/Link'
 import { styled } from '@mui/material/styles'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { useRouter } from 'next/router'
+import Dots from './Dots'
+import Image from "next/image";
 
-import HubSlider from './HubSlider'
 
 const { HubContext, NinaContext } = nina.contexts
 
@@ -24,19 +26,64 @@ const Hubs = () => {
     return Object.values(hubState)
   }, [hubState])
 
+
+  if (hubs.length === 0) {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '250px',
+        }}
+      >
+        <Dots size="80px" />
+      </Box>
+    )
+  }
+
   return (
     <HubsContainer overflowX="visible">
-      <Box sx={{ padding: { md: '40px 40px 140px 40px !important', xs: '30px 0px' } }}>
-        <Box sx={{ paddingLeft: { md: '30px', xs: '0' } }}>
+      <Box>
+        <Box sx={{ paddingLeft: { md: '15px', xs: '0' } }}>
           <Typography
-            variant="body1"
+            variant="h2"
             align="left"
+            fontWeight={'700'}
             className={classes.sectionHeader}
           >
             Hubs
           </Typography>
         </Box>
-        <HubSlider hubs={hubs} />
+        <HubGrid container >
+          {hubs?.map(hub => {
+            const imageUrl = hub?.json?.image;
+            console.log('hub :>> ', hub);
+            return(
+              <HubTile item md={4}>
+                {imageUrl &&
+                  <Link href={`/${hub.handle}`}>
+                    <a>
+                      <Image
+                        src={imageUrl}
+                        height={100}
+                        width={100}
+                        layout="responsive"
+                        priority={true}
+                        unoptimized={true}
+                        loading="eager"
+                      />
+                    </a>
+                  </Link>
+                }
+              </HubTile>
+            )
+          })}
+
+        </HubGrid>
+
+        
+        {/* <HubSlider hubs={hubs} /> */}
       </Box>
     </HubsContainer>
   )
@@ -52,27 +99,21 @@ const HubsContainer = styled('div')(({ theme }) => ({
   width: '1010px',
   margin: 'auto',
   overflowX: 'visible',
-  '& .MuiBox-root': {
-    paddingTop: '40px !important',
-  },
   [theme.breakpoints.down('md')]: {
     width: '80vw',
     marginBottom: '100px',
   },
-  [`& .${classes.sectionHeader}`]: {
-    fontWeight: '700 !important',
-    paddingBottom: `${theme.spacing(1)}`,
-    textTransform: 'uppercase !important',
-    position: 'relative',
-    '& .MuiTypography-root': {
-      textTransform: 'uppercase !important',
-      fontWeight: '700 !important',
-    },
-    '& .MuiButton-root': {
-      position: 'absolute',
-      top: '-10px',
-    },
-  },
 }))
+
+const HubGrid = styled(Grid)(() => ({
+  border: '2px solid red',
+  // padding: '15px 15px 15px'
+}))
+
+const HubTile = styled(Grid)(() => ({
+  border: '2px solid red',
+  padding: '15px 15px 15px'
+}))
+
 
 export default Hubs
---
+++ js/hubs/components/UserReleasesPrompt.js
@@ -0,0 +1,188 @@
+import React, {useState, useEffect, useContext, useMemo} from 'react'
+import nina from '@nina-protocol/nina-sdk'
+import {styled} from '@mui/material/styles'
+import {Box, Paper} from '@mui/material'
+import Modal from '@mui/material/Modal'
+import Backdrop from '@mui/material/Backdrop'
+import Fade from '@mui/material/Fade'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import Image from 'next/image'
+import Typography from '@mui/material/Typography'
+
+import {useSnackbar} from 'notistack'
+import Dots from './Dots'
+import {useWallet} from '@solana/wallet-adapter-react'
+
+const {HubContext, ReleaseContext, NinaContext} = nina.contexts
+
+const UserReleasesPrompt = ({userHubs, releasePubkey, metadata, hubPubkey, hubReleases}) => {
+  const [open, setOpen] = useState(false)
+  const {enqueueSnackbar} = useSnackbar()
+  const wallet = useWallet()
+
+  const {releaseState, getReleasesPublishedByUser, filterReleasesPublishedByUser} = useContext(ReleaseContext)
+  const {hubAddRelease, getHub} = useContext(HubContext)
+  const {collection} = useContext(NinaContext)
+  const [selectedHubId, setSelectedHubId] = useState()
+  const [inProgress, setInProgress] = useState(false)
+  const [canAddContent, setCanAddContent] = useState(false)
+  const [userPublishedReleases, setUserPublishedReleases] = useState([])
+  const [unpostedUserReleases, setUnpostedUserReleases] = useState([])
+
+
+  const userHasHubs = useMemo(() => userHubs && userHubs.length > 0, [userHubs])
+
+
+  useEffect(() => {
+    if (wallet?.connected) {
+      getReleasesPublishedByUser(wallet.publicKey)
+    }
+  }, [wallet?.connected])
+
+  useEffect(() => {
+    if (wallet?.connected) {
+      setUnpostedUserReleases(filterUnpostedReleases(hubReleases))
+    }
+  }, [releaseState, collection, hubReleases])
+
+  const filterUnpostedReleases =  (hubReleases) => {
+    let unposted;
+    if (hubReleases.length > 0) {
+      const userPostedReleases = filterReleasesPublishedByUser()    
+      unposted = userPostedReleases.filter(release => {
+        if (!hubReleases?.find(hr => hr.release === release.releasePubkey)) {
+          return release
+        }
+      }) 
+    } else {
+      unposted = filterReleasesPublishedByUser() 
+    }
+    return unposted
+  }
+
+  const handleRepost = async (release) => {
+    enqueueSnackbar(`Adding ${release.metadata.name} to Hub`, {
+      variant: 'info',
+    })
+    const result = await hubAddRelease(hubPubkey, release.releasePubkey)
+    if (result?.success) {
+      enqueueSnackbar(result.msg, {
+        variant: 'info',
+      })
+      getHub(hubPubkey)
+    } else {
+      enqueueSnackbar('Release not added to hub', {
+        variant: 'failure',
+      })
+    }
+  }
+
+  if (!unpostedUserReleases) {
+    return <></>
+  }
+
+  return (
+    <Root>
+        <ModalToggle
+          variant="outlined"
+          color="primary"
+          type="submit"
+          onClick={() => setOpen(true)}
+          sx={{height: '22px', width: '28px', m: 0}}
+          fullWidth
+        >
+          Click to add your existing releases to your hub
+        </ModalToggle>
+
+      <StyledModal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={() =>setOpen(false)}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <StyledPaper>
+            <Typography variant="h4" mb={1}>
+              Click on a Release below to add it to your hub:
+            </Typography>
+            <Grid container spacing={1}>
+              {unpostedUserReleases?.map(release => {
+                return (
+                  <Grid item md={4} key={}>
+                    <ReleaseImage
+                      width={50}
+                      height={50}
+                      layout="responsive"
+                      src={release.metadata.image}
+                      priority={true}
+                      unoptimized={true}
+                      loading="eager"
+                      onClick={() => handleRepost(release)}
+                    />
+                  </Grid>
+                )
+              })}
+            </Grid>
+
+          </StyledPaper>
+        </Fade>
+      </StyledModal>
+    </Root>
+  )
+}
+
+
+const Root = styled('div')(({theme}) => ({
+  display: 'flex',
+  alignItems: 'center',
+  width: '100%',
+  padding : '0 15px'
+}))
+
+const ModalToggle = styled(Button)(({theme}) => ({
+  color: `${theme.palette.text.primary} !important`,
+  width: '100% !important',
+  border: '2px solid red',
+  ':disabled': {
+    color: theme.palette.text.primary + 'a0',
+  },
+  '&:hover': {
+    opacity: '50%',
+    backgroundColor: `${theme.palette.transparent} !important`,
+  },
+}))
+
+const StyledModal = styled(Modal)(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}))
+
+const StyledPaper = styled(Paper)(({theme}) => ({
+  backgroundColor: theme.palette.background.paper,
+  border: '2px solid #000',
+  boxShadow: theme.shadows[5],
+  padding: `30px 60px 45px`,
+  width: '40vw',
+  maxHeight: '90vh',
+  overflowY: 'auto',
+  zIndex: '10',
+  display: 'flex',
+  flexDirection: 'column',
+  minWidth: '600px',
+}))
+
+const ReleaseImage = styled(Image)(({theme}) => ({
+  border: '2px solid red',
+  '&:hover': {
+    opacity: '50%'
+  }
+}))
+
+export default UserReleasesPrompt
