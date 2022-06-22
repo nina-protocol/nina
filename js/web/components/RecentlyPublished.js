c37e13ce778f77593cc22f1d6667c2da50bac997
facing-n <mike@nina.market>
2022-06-22 13:59:56 -0400

    .

--- js/web/components/RecentlyPublished.js
+++ js/web/components/RecentlyPublished.js
@@ -92,7 +92,8 @@ const RecentlyPublished = (props) => {
                         height={100}
                         width={100}
                         layout="responsive"
-                        priority={true}
+                        priority={false}
+                        lazyLoad={true}
                         unoptimized={true}
                       />
                     </a>
