ba1e1d38b1cc6772589c52fff9e833a39855d3c4
Eric Farber <eric.j.farber@gmail.com>
2022-06-27 16:00:37 -0400

    mainnet

--- js/hubs/next.config.js
+++ js/hubs/next.config.js
@@ -4,8 +4,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-// const cluster = 'mainnet-beta'
-const cluster = 'devnet'
+const cluster = 'mainnet-beta'
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
