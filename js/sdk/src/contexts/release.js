0a6c692a802d51e9406160261cf9b9a0c20fd5b4
facing-n <mike@nina.market>
2022-06-27 19:45:58 -0400

    include hub fee in swap

--- js/sdk/src/contexts/release.js
+++ js/sdk/src/contexts/release.js
@@ -508,9 +508,9 @@ const releaseContextHelper = ({
         });
         instructions.push(additionalComputeBudgetInstruction)
         const solPrice = await getSolPrice()
-        const releaseUiPrice = ninaClient.nativeToUi(release.price.toNumber(), ids.mints.usdc) - usdcBalance
+        const releaseUiPrice = ninaClient.nativeToUi(release.price.toNumber() + (release.price.toNumber() * hub.referralFee.toNumber()), ids.mints.usdc) - usdcBalance
         const { data } = await axios.get(
-          `https://quote-api.jup.ag/v1/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=${ninaClient.uiToNative((releaseUiPrice + (releaseUiPrice * .02)) / solPrice, ids.mints.wsol)}&slippage=0.5&onlyDirectRoutes=true`
+          `https://quote-api.jup.ag/v1/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=${ninaClient.uiToNative((releaseUiPrice + (releaseUiPrice * .01)) / solPrice, ids.mints.wsol)}&slippage=0.5&onlyDirectRoutes=true`
         )
         let transactionInstructions
         for await (let d of data.data) {
@@ -809,7 +809,7 @@ const releaseContextHelper = ({
         const solPrice = await getSolPrice()
         const releaseUiPrice = ninaClient.nativeToUi(release.price.toNumber(), ids.mints.usdc) - usdcBalance
         const { data } = await axios.get(
-          `https://quote-api.jup.ag/v1/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=${ninaClient.uiToNative((releaseUiPrice + (releaseUiPrice * .02)) / solPrice, ids.mints.wsol)}&slippage=0.5&feeBps=50&onlyDirectRoutes=true`
+          `https://quote-api.jup.ag/v1/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=${ninaClient.uiToNative((releaseUiPrice + (releaseUiPrice * .01)) / solPrice, ids.mints.wsol)}&slippage=0.5&feeBps=50&onlyDirectRoutes=true`
         )
         const transactions = await axios.post(
           'https://quote-api.jup.ag/v1/swap', {
