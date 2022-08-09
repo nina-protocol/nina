54b96333c76cea6a2209381a6fae3aeac8542fd5
facing-n <mike@nina.market>
2022-08-09 14:14:44 -0400

    add back autoconnect to sanity check

--- js/hubs/components/Navigation.js
+++ js/hubs/components/Navigation.js
@@ -111,12 +111,12 @@ const Navigation = ({ hubPubkey }) => {
     return base58.slice(0, 4) + ".." + base58.slice(-4);
   }, [wallet, base58]);
 
-  useEffect(() => {
-    console.log('wallet: ', wallet)
-    if (wallet.wallet && !wallet.publicKey && !wallet.connecting && !wallet.connected) {
-      wallet.connect()
-    }
-  }, [wallet])
+  // useEffect(() => {
+  //   console.log('wallet: ', wallet)
+  //   if (wallet.wallet && !wallet.publicKey && !wallet.connecting && !wallet.connected) {
+  //     wallet.connect()
+  //   }
+  // }, [wallet])
 
   const canAddContent = useMemo(() => {
     if (wallet?.connected && hubPubkey) {
