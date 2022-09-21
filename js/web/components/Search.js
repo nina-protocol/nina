16a3aab48aab014f5a5a6a03e7f35320b6c9629d
ikjudd <ianjudd@Ians-Air.lan>
2022-09-21 14:32:15 -0400

    leftalign search

--- js/web/components/Search.js
+++ js/web/components/Search.js
@@ -5,24 +5,27 @@ import TextField from '@mui/material/TextField'
 import { Box } from '@mui/system'
 import { Typography } from '@mui/material'
 import { useCallback } from 'react'
-import { useAutocomplete } from '@mui/base/AutocompleteUnstyled';
+import { useAutocomplete } from '@mui/base/AutocompleteUnstyled'
+import Dots from './Dots'
+
 
 const Search = () => {
-    const {
-        getRootProps,
-        getInputLabelProps,
-        getInputProps,
-        getListboxProps,
-        getOptionProps,
-        groupedOptions,
-    } = useAutocomplete({
-        id: 'nina-search',
-        options: [response],
-        getOptionLabel: (option) => option
-    })
+  // const {
+  //     getRootProps,
+  //     getInputLabelProps,
+  //     getInputProps,
+  //     getListboxProps,
+  //     getOptionProps,
+  //     groupedOptions,
+  // } = useAutocomplete({
+  //     id: 'nina-search',
+  //     options: [response],
+  //     getOptionLabel: (option) => option
+  // })
 
   const [query, setQuery] = useState('')
   const [response, setResponse] = useState(undefined)
+  const [fetchedResponse, setFetchedResponse] = useState(undefined)
 
   useEffect(() => {
     NinaSdk.client.init(
@@ -30,25 +33,33 @@ const Search = () => {
       process.env.SOLANA_CLUSTER_URL,
       process.env.NINA_PROGRAM_ID
     )
-    console.log('NinaSdk', NinaSdk.Search)
-    console.log('cool')
   }, [])
 
-  const updateResponse = useCallback(res => {
-    console.log('response', response)
-  }, [response])
+  const updateResponse = useCallback(
+    (res) => {
+      console.log('response', response)
+    },
+    [response]
+  )
+
   const handleSubmit = async (e) => {
     e.preventDefault()
     e.stopPropagation()
-
-    setQuery(e.target.value)
-    await NinaSdk.Search.withQuery(query).then(setResponse).then(setQuery(''))
-    updateResponse()
+    setFetchedResponse(false)
+    if (e.target.value !== null || e.target.value !== '') {
+      setQuery(e.target.value)
+      await NinaSdk.Search.withQuery(query).then(setResponse)
+      setFetchedResponse(true)
+    }
+    if (response) {
+      setQuery('')
+      updateResponse()
+    }
     console.log('query', query)
     console.log('response', response)
   }
   return (
-    <Box>
+    <Box sx={{ height: '60vh', width: '960px' }}>
       <Form onSubmit={(e) => handleSubmit(e)}>
         <SearchInputWrapper>
           <TextField
@@ -58,43 +69,76 @@ const Search = () => {
             label="Search for anything..."
             id="fullWidth"
             variant="standard"
-            {...getInputProps()}
+            value={query}
           />
-        
         </SearchInputWrapper>
       </Form>
-      <Box>
-        {/* <Box>
-            <Typography sx={{fontWeight: 'bold'}}>ARTISTS</Typography>
-            {
-            response?.artists.map((artist) => (
+      <SearchResultsWrapper>
+        <ResponsiveSearchResultContainer>
+
+          {fetchedResponse === false && (
+            <Box>
+              <Dots />
+            </Box>
+          )}
+          {fetchedResponse && response.artists.length > 0 && (
+            <>
+              <Typography sx={{ fontWeight: 'bold' }}>ARTISTS</Typography>
+              {response?.artists.map((artist) => (
                 <Typography>{artist.name}</Typography>
-            ))
-        }
-        </Box>
-      <Box>
-        <Typography sx={{fontWeight: 'bold'}}>RELEASES</Typography>
-        {
-            response?.releases.map((release) =>(
+              ))}
+            </>
+          )}
+        </ResponsiveSearchResultContainer>
+        <ResponsiveSearchResultContainer>
+          {fetchedResponse === false && (
+            <Box>
+              <Dots />
+            </Box>
+          )}
+          {fetchedResponse && response.releases.length > 0 && (
+            <>
+              <Typography sx={{ fontWeight: 'bold' }}>RELEASES</Typography>
+              {response?.releases.map((release) => (
                 <Typography>{release.title}</Typography>
-            ))
-        }
-      </Box>
-      <Box>
-        <Typography sx={{fontWeight: 'bold'}}>HUBS</Typography>
-        {
-            response?.hubs.map((hub) =>(
+              ))}
+            </>
+          )}
+        </ResponsiveSearchResultContainer>
+        <ResponsiveSearchResultContainer>
+          {fetchedResponse === false && (
+            <Box>
+              <Dots />
+            </Box>
+          )}
+          {fetchedResponse && response.hubs.length > 0 && (
+            <>
+              <Typography sx={{ fontWeight: 'bold' }}>HUBS</Typography>
+              {response?.hubs.map((hub) => (
                 <Typography>{hub.displayName}</Typography>
-            ))
-        }
-      </Box> */}
-      </Box>
+              ))}
+            </>
+          )}
+        </ResponsiveSearchResultContainer>
+      </SearchResultsWrapper>
     </Box>
   )
 }
 
-const SearchInputWrapper = styled(Box)(({ theme }) => ({}))
+const SearchInputWrapper = styled(Box)(({ theme }) => ({
+  maxWidth: '960px'
+}))
 const Form = styled('form')(({ theme }) => ({
-  width: '50vw',
+  
+}))
+const SearchResultsWrapper = styled(Box)(({theme}) => ({
+  textAlign: 'left'
+}))
+const ResponsiveSearchResultContainer = styled(Box)(({ theme }) => ({
+  maxHeight: '60vh',
+  width: '960px',
+  overflow: 'auto',
+  webkitOverflowScrolling: 'touch',
+  padding: '10px 0'
 }))
 export default Search
