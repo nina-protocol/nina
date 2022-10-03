571b34e43ac222c0026cbc6f41467dd350fe3799
facing-n <mike@nina.market>
2022-10-03 18:13:17 -0400

    Hubs meta tags

--- js/hubs/pages/[hubPubkey]/dashboard.js
+++ js/hubs/pages/[hubPubkey]/dashboard.js
@@ -1,12 +1,26 @@
 import Dashboard from "../../components/Dashboard";
 import axios from "axios";
 
-const DashboardPage = ({ hubPubkey }) => {
+const DashboardPage = ({ hubPubkey, hub }) => {
   return (
     <>
+      <Head>
+        <title>{`Nina Hubs - ${hub?.data.displayName} Dashboard`}</title>
+        <meta name="og:type" content="website" />
+        <meta
+          name="description"
+          content={`Hubs. Powered by Nina.`} />
+        <meta name="og:image" content={hub?.json.image} />    
+        <meta name="twitter:image:type" content="image/png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@ninaprotocol" />
+        <meta name="twitter:creator" content="@ninaprotocol" />
+        <meta name="twitter:image" content="https://hubs.ninaprotocol.com/images/nina-blue.png" />
+        <meta name="og:image" href="https://hubs.ninaprotocol.com/images/nina-blue.png"  />      
+      </Head>
       <Dashboard hubPubkey={hubPubkey} />
     </>
-  );
+  )
 };
 
 DashboardPage.getInitialProps = async (context) => {
--- js/hubs/pages/create.js
+++ js/hubs/pages/create.js
@@ -1,7 +1,39 @@
 import HubCreate from "../components/HubCreate";
+import Head from "next/head";
 
 const CreateHubPage = () => {
-  return <HubCreate update={false} />;
+  return (
+    <>
+      <Head>
+        <title>Nina Hubs - Create</title>
+        <meta
+          name="description"
+          content={"Nina Protocol is a digitally native music ecosystem"}
+        />
+        <meta name="og:type" content="website" />
+        <meta 
+          name="og:title"
+          content="Nina Hubs - Create"
+        />
+        <meta
+          name="og:description"
+          content={"Nina Protocol is a digitally native music ecosystem"}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@ninaprotocol" />
+        <meta name="twitter:creator" content="@ninaprotocol" />
+        <meta name="twitter:image:type" content="image/png" />
+        <meta
+          name="twitter:title"
+          content="Nina Hubs - Create"
+        />
+        <meta name="twitter:description" content={"Nina Protocol is a digitally native music ecosystem"} />
+        <meta name="twitter:image" content="https://ninaprotocol.com/images/nina-blue.png" />
+        <meta name="og:image" href="https://ninaprotocol.com/images/nina-blue.png" />
+      </Head>
+      <HubCreate update={false} />;
+    </>
+  );
 };
 
 export default CreateHubPage;
