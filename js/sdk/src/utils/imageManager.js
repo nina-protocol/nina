ed1b4cce0067db4471d17c0f10facd1178110c56
facing-n <mike@nina.market>
2022-10-17 12:13:44 -0400

    if img > yesterday dont use imgix

--- js/sdk/src/utils/imageManager.js
+++ js/sdk/src/utils/imageManager.js
@@ -1,28 +1,31 @@
-import ImgixClient from '@imgix/js-core';
+import ImgixClient from '@imgix/js-core'
 
-export const getImageFromCDN = (url, width=400, date) => {
+export const getImageFromCDN = (url, width = 400, date) => {
   let image = url
   const now = new Date()
-  if (((now - date) / 1000 > 120)) {
+  const yesterday = new Date(today)
+  yesterday.setDate(yesterday.getDate() - 1)
+
+    if (date < yesterday) {
     const client = new ImgixClient({
       domain: process.env.IMGIX_URL,
       secureURLToken: process.env.NEXT_PUBLIC_IMGIX_TOKEN,
-    });
-  
+    })
+
     image = client.buildURL(url, {
       width,
-      fm: 'webp'
-    });  
+      fm: 'webp',
+    })
   }
 
   return image
 }
 
-export const loader = ({src, width}) => {
+export const loader = ({ src, width }) => {
   const url = new URL(src)
   if (src.includes('&s=')) {
     let fixedURL = src.replace(`&s=${url.searchParams.get('s')}`, '')
-    return fixedURL += `&s=${url.searchParams.get('s')}`
+    return (fixedURL += `&s=${url.searchParams.get('s')}`)
   }
   return url
 }
