15595d119d43ede6e6489dbec0fc19a30b31d4ad
ikjudd <ianjudd@Ians-Air.lan>
2022-10-27 15:19:28 -0400

    sync

--- js/web/next.config.js
+++ js/web/next.config.js
@@ -69,9 +69,6 @@ const moduleExports = withTM({
   env: {
     IMGIX_URL,
     NEXT_PUBLIC_IMGIX_TOKEN,
-    NINA_API_ENDPOINT: process.env.NINA_API_ENDPOINT,
-    SOLANA_CLUSTER_URL: process.env.SOLANA_CLUSTER_URL,
-    NINA_PROGRAM_ID: process.env.NINA_PROGRAM_ID,
     REACT_APP_CLUSTER: cluster,
     NINA_API_ENDPOINT: process.env.NINA_API_ENDPOINT,
     NINA_PROGRAM_ID: process.env.NINA_PROGRAM_ID,
