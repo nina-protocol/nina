73e459da9f9cdad86dd326ed1652f170accf544c
Eric Farber <eric.j.farber@gmail.com>
2022-11-03 13:37:12 -0400

    update metatags

--- js/web/pages/hubs/index.js
+++ js/web/pages/hubs/index.js
@@ -11,7 +11,7 @@ const AllHubsPage = () => {
           content={'Nina Protocol is a digitally native music ecosystem'}
         />
         <meta name="og:type" content="website" />
-        <meta name="og:title" content="Nina Hubs - All Hubs" />
+        <meta name="og:title" content="Nina - All Hubs" />
         <meta
           name="og:description"
           content={'Nina Protocol is a digitally native music ecosystem'}
@@ -20,7 +20,7 @@ const AllHubsPage = () => {
         <meta name="twitter:site" content="@ninaprotocol" />
         <meta name="twitter:creator" content="@ninaprotocol" />
         <meta name="twitter:image:type" content="image/png" />
-        <meta name="twitter:title" content="Nina Hubs - All Hubs" />
+        <meta name="twitter:title" content="Nina - All Hubs" />
         <meta
           name="twitter:description"
           content={'Nina Protocol is a digitally native music ecosystem'}
