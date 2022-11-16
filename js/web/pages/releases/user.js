f95bef5cca649d263509e3fd4ebb8c9dda8f7029
facing-n <mike@nina.market>
2022-11-16 13:25:19 -0500

    fix datetime in Hubs Slider on Home - hubs app

---
+++ js/web/pages/releases/user.js
@@ -0,0 +1,42 @@
+import ReleaseList from '../../components/ReleaseList'
+import Head from 'next/head'
+
+const UserReleasePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Nina Protocol - My Releases</title>
+        <meta
+          name="description"
+          content={'Nina Protocol is a digitally native music ecosystem'}
+        />
+        <meta name="og:type" content="website" />
+        <meta name="og:title" content="Nina Protocol - My Releases" />
+        <meta
+          name="og:description"
+          content={'Nina Protocol is a digitally native music ecosystem'}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@ninaprotocol" />
+        <meta name="twitter:creator" content="@ninaprotocol" />
+        <meta name="twitter:image:type" content="image/png" />
+        <meta name="twitter:title" content="Nina Protocol - My Releases" />
+        <meta
+          name="twitter:description"
+          content={'Nina Protocol is a digitally native music ecosystem'}
+        />
+        <meta
+          name="twitter:image"
+          content="https://ninaprotocol.com/images/nina-blue.png"
+        />
+        <meta
+          name="og:image"
+          href="https://ninaprotocol.com/images/nina-blue.png"
+        />
+      </Head>
+      <ReleaseList />
+    </>
+  )
+}
+
+export default UserReleasePage
