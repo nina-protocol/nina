528db1d791b59cb19a82de7732faf80890a5c357
facing-n <mike@nina.market>
2022-11-18 16:44:01 -0500

    test

--- js/sdk/src/utils/sdkInit.js
+++ js/sdk/src/utils/sdkInit.js
@@ -1,8 +1,8 @@
 import NinaSdk from '@nina-protocol/js-sdk'
 
-export const initSdkIfNeeded = async () => {
-  if (!NinaSdk.client.program) {
-    if (process.env.USE_KEY === 'true') {
+export const initSdkIfNeeded = async (override) => {
+  if (!NinaSdk.client.program || override) {
+    if (override || process.env.USE_KEY === 'true') {
       await NinaSdk.client.init(
         process.env.NINA_API_ENDPOINT,
         process.env.SOLANA_CLUSTER_URL_BUILD,
@@ -12,7 +12,7 @@ export const initSdkIfNeeded = async () => {
     } else {
       await NinaSdk.client.init(
         process.env.NINA_API_ENDPOINT,
-        process.env.SOLANA_CLUSTER_URL,
+        process.env.SOLANA_CLUSTER_URL_BUILD,
         process.env.NINA_PROGRAM_ID
       );
     }
