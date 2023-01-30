e43257c8cf977c24e42db9707c2f72fe83ee0356
facing-n <mike@nina.market>
2023-01-30 18:19:56 -0500

    add devnet indicator

---
+++ js/sdk/src/components/DevnetIndicator.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { styled } from '@mui/material/styles'
+
+const DevnetIndicator = ({ devnet }) => {
+  if (process.env.SOLANA_CLUSTER === 'devnet') {
+    return (
+      <DevnetIndicatorStyled>[DEVNET]</DevnetIndicatorStyled>
+    )
+  } else {
+    return null
+  }
+}
+
+const DevnetIndicatorStyled = styled('span')(({ theme }) => ({
+  color: theme.palette.red,
+  marginLeft: theme.spacing(1),
+  fontFamily: 'monospace'
+}))
+
+export default DevnetIndicator
