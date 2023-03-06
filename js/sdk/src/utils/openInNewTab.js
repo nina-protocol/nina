be8480534b3b9d40d8ec63a97fd9764b0c011b95
ikjudd <iankimjudd@gmail.com>
2023-03-06 14:51:22 -0500

    formatted

--- js/sdk/src/utils/openInNewTab.js
+++ js/sdk/src/utils/openInNewTab.js
@@ -1,6 +1,6 @@
 export default function openInNewTab(event, window, url) {
-    if (event.ctrlKey || event.metaKey) {
-      const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-      if (newWindow) newWindow.opener = null
-    }
+  if (event.ctrlKey || event.metaKey) {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
 }
