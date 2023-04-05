3c336eed014821de7a803ac880defb417b5831a7
ikjudd <iankimjudd@gmail.com>
2023-04-05 18:14:27 -0400

    blacklist component added

--- js/web/components/Admin.js
+++ js/web/components/Admin.js
@@ -16,6 +16,7 @@ const Admin = () => {
   const walletPubkey = wallet.publicKey?.toBase58()
   const hasAccess = onboardingCodeWhitelist.includes(walletPubkey)
   const [code, setCode] = useState()
+  const [releaseId, setReleaseId] = useState()
   const { getSolBalanceForPublicKey } = useContext(Nina.Context)
   const [verificationBalance, setVerificationBalance] = useState(0)
   const [dispatcherBalance, setDispatcherBalance] = useState(0)
@@ -100,7 +101,7 @@ const Admin = () => {
                 ? `Verification Wallet Balance: ${verificationBalance} SOL`
                 : 'Verification Wallet Balance: 0 SOL'}
             </Typography>
-            <Typography variant="h4" mb={2}>
+            <Typography variant="h4" mb={4}>
               {dispatcherBalance > 0
                 ? `Dispatcher Wallet Balance: ${dispatcherBalance} SOL`
                 : 'Dispatcher Wallet Balance: 0 SOL'}
@@ -114,6 +115,7 @@ const Admin = () => {
           <Typography for="code" mb={1}>
             If generated successfully, onboarding Code should print below
           </Typography>
+
           <Input
             type="text"
             id="code"
@@ -122,6 +124,29 @@ const Admin = () => {
             onChange={(event) => setCode(event.target.value)}
             sx={{ width: '25vw' }}
           />
+
+          <Typography for="code" mt={4} mb={1}>
+            If you have a release that needs to be blacklisted, please enter its
+            ID below.
+          </Typography>
+          <Input
+            type="text"
+            id="code"
+            name="code"
+            value={releaseId}
+            onChange={(event) => setReleaseId(event.target.value)}
+            sx={{ width: '25vw' }}
+          />
+          <Box mt={1}>
+            <Button
+              variant="outlined"
+              onClick={() => {
+                console.log('releaseId', releaseId)
+              }}
+            >
+              Blacklist Release
+            </Button>
+          </Box>
         </Box>
       )}
     </ScrollablePageWrapper>
