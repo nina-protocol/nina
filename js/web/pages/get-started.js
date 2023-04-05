55bc0e71365ee4713a92d34f3a3a2ad100a6f74c
ikjudd <iankimjudd@gmail.com>
2023-04-05 13:46:29 -0400

    moved getstarted out of getstarted folder

--- js/web/pages/get-started.js
+++ js/web/pages/get-started.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Onboard from '../../components/Onboard'
+import Onboard from '../components/Onboard'
 const GetStartedPage = () => {
   return (
     <>
