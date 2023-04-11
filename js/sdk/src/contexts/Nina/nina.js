ed837619bc7af83083f678bc163497b2025ce81d
facing-n <mike@ninaprotocol.com>
2023-04-11 17:47:01 -0400

    wallet fix

--- js/sdk/src/contexts/Nina/nina.js
+++ js/sdk/src/contexts/Nina/nina.js
@@ -878,7 +878,7 @@ const ninaContextHelper = ({
         const bundlrInstance = new module.WebBundlr(
           bundlrHttpAddress,
           'solana',
-          provider.wallet.wallet.adapter,
+          provider.wallet,
           {
             providerUrl: process.env.SOLANA_CLUSTER_URL_BUNDLR,
             timeout: 1000000000000000,
