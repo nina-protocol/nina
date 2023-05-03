49dc67d03fc9b11140e451ae217927c51e540141
ikjudd <iankimjudd@gmail.com>
2023-05-03 14:48:03 -0400

    formatted

--- js/sdk/src/components/EmailOTPForm.js
+++ js/sdk/src/components/EmailOTPForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { MuiOtpInput } from 'mui-one-time-password-input'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
