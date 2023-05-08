163ca53ea3a87640292f670bf17cbadebd242eea
Eric Farber <eric.j.farber@gmail.com>
2023-05-08 11:31:47 -0400

    Artist Program page (#995)
    * artist program page
    
    * mobile style
    
    * format
    
    * format
    
    * rename

---
+++ js/web/components/ArtistProgram.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import { styled } from '@mui/system'
+import EmailCapture from '@nina-protocol/nina-internal-sdk/esm/EmailCapture'
+
+const ArtistProgram = () => {
+  return (
+    <Root>
+      <Box sx={{ mb: 1 }}>
+        <Typography variant="h3">Nina Artist Program</Typography>
+      </Box>
+
+      <EmailCapture forceOpen={true} inArtistProgram={true} />
+    </Root>
+  )
+}
+
+const Root = styled(Box)(({ theme }) => ({
+  textAlign: 'left',
+  minHeight: '50vh',
+  width: '550px',
+  [theme.breakpoints.down('md')]: {
+    width: '90vw',
+    margin: '0 auto',
+  },
+}))
+
+export default ArtistProgram
