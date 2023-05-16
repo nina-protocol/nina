042da59478a31606abf86dabecb9b1aec1788e4c
facing-n <mike@nina.market>
2023-05-16 11:26:39 -0400

    metatgs (#1067)

--- js/web/pages/nap.js
+++ js/web/pages/nap.js
@@ -9,22 +9,22 @@ const ArtistProgramPage = () => {
         <title>Nina Protocol - Artist Program</title>
         <meta
           name="description"
-          content={'Nina Protocol is a digitally native music ecosystem'}
+          content={'Nina Protocol is an independent music ecosystem'}
         />
         <meta name="og:type" content="website" />
         <meta name="og:title" content="Nina Protocol - Artist Program" />
         <meta
           name="og:description"
-          content={'Nina Protocol is a digitally native music ecosystem'}
+          content={'Nina Protocol is an independent music ecosystem'}
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@ninaprotocol" />
         <meta name="twitter:creator" content="@ninaprotocol" />
         <meta name="twitter:image:type" content="image/png" />
-        <meta name="twitter:title" content="Nina Protocol - Learn More" />
+        <meta name="twitter:title" content="Nina Protocol - Artist Program" />
         <meta
           name="twitter:description"
-          content={'Nina Protocol is a digitally native music ecosystem'}
+          content={'Nina Protocol is an independent music ecosystem'}
         />
 
         <meta
--- js/web/pages/start.js
+++ js/web/pages/start.js
@@ -6,25 +6,25 @@ const StartPage = () => {
   return (
     <>
       <Head>
-        <title>Nina Protocol - Get Started</title>
+        <title>Nina Protocol - Start</title>
         <meta
           name="description"
-          content={'Nina Protocol is a digitally native music ecosystem'}
+          content={'Nina Protocol is an independent music ecosystem'}
         />
         <meta name="og:type" content="website" />
-        <meta name="og:title" content="Nina Protocol - Get Started" />
+        <meta name="og:title" content="Nina Protocol - Start" />
         <meta
           name="og:description"
-          content={'Nina Protocol is a digitally native music ecosystem'}
+          content={'Nina Protocol is an independent music ecosystem'}
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@ninaprotocol" />
         <meta name="twitter:creator" content="@ninaprotocol" />
         <meta name="twitter:image:type" content="image/png" />
-        <meta name="twitter:title" content="Nina Protocol - Get Started" />
+        <meta name="twitter:title" content="Nina Protocol - Start" />
         <meta
           name="twitter:description"
-          content={'Nina Protocol is a digitally native music ecosystem'}
+          content={'Nina Protocol is an independent music ecosystem'}
         />
 
         <meta
