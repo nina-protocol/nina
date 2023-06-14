22c6e306e74ee9e969823f4d1292e24b8e58ce65
facing-n <mike@ninaprotocol.com>
2023-06-14 13:16:56 -0400

    sold -> collected on open editions

--- js/web/components/ReleasePurchase.js
+++ js/web/components/ReleasePurchase.js
@@ -275,7 +275,7 @@ const ReleasePurchase = (props) => {
               sx={{ color: '#black !important' }}
             >
               Open Edition:{' '}
-              {`${release?.saleCounter > 0 ? release?.saleCounter : 0} Sold`}
+              {`${release?.saleCounter > 0 ? release?.saleCounter : 0} Collected`}
             </Typography>
           ) : (
             <>
