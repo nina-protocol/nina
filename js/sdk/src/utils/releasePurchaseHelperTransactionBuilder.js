a4e5b5a62c025ed063628397071ca4d8240f43e8
facing-n <mike@ninaprotocol.com>
2023-10-20 12:38:21 -0400

    fix posts in program

--- js/sdk/src/utils/releasePurchaseHelperTransactionBuilder.js
+++ js/sdk/src/utils/releasePurchaseHelperTransactionBuilder.js
@@ -1,4 +1,4 @@
-import * as anchor from '@project-serum/anchor'
+import * as anchor from '@coral-xyz/anchor'
 import { findOrCreateAssociatedTokenAccount } from './web3'
 
 import { decodeNonEncryptedByteArray } from './encrypt'
